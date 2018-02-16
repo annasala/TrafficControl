6638ab4789968a97c6dbc1ac4d47bd136c6fbe74
annasala <anna.emilia.sala@gmail.com>
2018-02-16 21:10:03 +0100

    UDP socket added; sending added

--- src/application/TrafficHandlerThread.java
+++ src/application/TrafficHandlerThread.java
@@ -1,13 +1,23 @@
 package application;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
 public class TrafficHandlerThread implements Runnable{
 
 	String address;
-	String port;
+	int port;
 	String throughput;
 	String packet;
 	private boolean isStopped = false;
-	
+	private static final int SOURCE_PORT = 6010;
+	InetAddress IPAddress;
+    DatagramSocket serverSocket;
+    byte[] sendData;
 	
 	
 	public synchronized void setStopped(boolean isStopped) {
@@ -19,17 +29,43 @@ public class TrafficHandlerThread implements Runnable{
 	public TrafficHandlerThread(String address, String port, String throughput, String packet) {
 		super();
 		this.address = address;
-		this.port = port;
+		this.port = Integer.parseInt(port);
 		this.throughput = throughput;
 		this.packet = packet;
+		this.sendData= new byte[1024];
+		
+		for(int r = 0; r<sendData.length;r++){
+			sendData[r]=(byte)0xAA;
+		}
+		
+		
+		try {
+			this.IPAddress=InetAddress.getByName(address);
+			this.serverSocket=new DatagramSocket(SOURCE_PORT);
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (SocketException s){
+			
+			s.printStackTrace();
+		}
 	}
 
 
 
 	@Override
 	public void run() {
+		DatagramPacket sendPacket= new DatagramPacket(sendData, sendData.length, IPAddress, port);
+		
 		System.out.println("Thread has started!");
 		while(isStopped()!=true){
+			try {
+				serverSocket.send(sendPacket);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			}
 		System.out.println("Thread stop!");
 	}
